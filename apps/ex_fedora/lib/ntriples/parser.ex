6c829353049e8e35f5e3e07d2b4d10baa6ff07b1
Trey Pendragon <tpendragon@princeton.edu>
2016-02-28 13:51:08 -0800

    Faster parser

--- apps/ex_fedora/lib/ntriples/parser.ex
+++ apps/ex_fedora/lib/ntriples/parser.ex
@@ -1,18 +1,14 @@
 require IEx
 defmodule NTriples.Parser do
   alias RDF.Literal
-  @ntriples_regex ~r/(?<subject><[^\s]+>|_:([A-Za-z][A-Za-z0-9\-_]*))[
-  ]*(?<predicate><[^\s]+>)[
-  ]*(?<object><[^\s]+>|_:([A-Za-z][A-Za-z0-9\-_]*)|"(?<literal_string>(?:\\"|[^"])*)"(@(?<literal_language>[a-z]+[\-A-Za-z0-9]*)|\^\^<(?<literal_type>[^>]+)>)?)[ ]*./i
+  @ntriples_regex ~r/(?<subject><(?<subject_uri>[^\s]+)>|_:([A-Za-z][A-Za-z0-9\-_]*))[
+  ]*(?<predicate><(?<predicate_uri>[^\s]+)>)[
+  ]*(?<object><(?<object_uri>[^\s]+)>|_:([A-Za-z][A-Za-z0-9\-_]*)|"(?<literal_string>(?:\\"|[^"])*)"(@(?<literal_language>[a-z]+[\-A-Za-z0-9]*)|\^\^<(?<literal_type>[^>]+)>)?)[ ]*./i
   def parse(content) do
     content
     |> String.split(".\n")
     |> Enum.map(&capture_triple_map/1)
     |> Enum.reduce(%{}, &process_capture/2)
-  end
-
-  defp process_capture(nil, accumulator) do
-    accumulator
   end
 
   defp capture_triple_map("") do
@@ -23,13 +19,17 @@ defmodule NTriples.Parser do
     Regex.named_captures(@ntriples_regex, string)
   end
 
+  defp process_capture(nil, accumulator) do
+    accumulator
+  end
+
   defp process_capture(map, accumulator) when map == %{} do
     accumulator
   end
 
   defp process_capture(capture_map, accumulator) do
-    subject = process_subject(capture_map["subject"])
-    predicate = process_subject(capture_map["predicate"])
+    subject = process_subject(capture_map)
+    predicate = process_predicate(capture_map)
     object = process_object(capture_map)
     append_triple(accumulator, {subject, predicate, object})
   end
@@ -42,21 +42,27 @@ defmodule NTriples.Parser do
       %{^subject => %{^predicate => existing_value}} ->
         new_value = [existing_value, object]
         Map.put(map, subject, Map.merge(map[subject], %{predicate => new_value}))
-      %{^subject => existing_subject_graph} ->
+      %{^subject => _} ->
         Map.put(map, subject, Map.merge(map[subject], %{predicate => object}))
       _ ->
         Map.put(map, subject, %{predicate => object})
     end
   end
 
-  defp process_subject("<" <> subject) do
-    subject
-    |> String.rstrip(?>)
+  defp process_subject(%{"subject_uri" => subject_uri}) do
+    subject_uri
   end
 
-  defp process_object(%{"object" => "<" <> object_uri}) do
-    uri = process_subject("<" <> object_uri)
-    %{"@id" => uri}
+  defp process_predicate(%{"predicate_uri" => predicate_uri}) do
+    predicate_uri
+  end
+
+  defp process_object(map = %{"object_uri" => ""}) do
+    process_object(Map.drop(map, ["object_uri"]))
+  end
+
+  defp process_object(%{"object_uri" => object_uri}) do
+    %{"@id" => object_uri}
   end
 
   defp process_object(%{"literal_string" => value, "literal_language" => language}) do
